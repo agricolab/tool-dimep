23518b08accdc359ad8485e37dd1f9483960865d
agricolab <robertsadresse@gmx.de>
2020-10-09 15:44:33 +0200

    Test version.py

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -7,3 +7,9 @@ import pytest
 def test_(traces, algo):
     for trace in traces:
         globals()[algo](trace, tms_sampleidx=1000)
+
+
+def test_version():
+    from dimep.api import version
+
+    assert type(version) == str
